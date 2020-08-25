1d0c9d13adb1069400130fe101ccd5130ea79034
vivek-dev-26 <k.vivek@successive.tech>
2020-08-25 15:21:12 +0530

    Initial commit

---
+++ brigade.js
@@ -0,0 +1,35 @@
+const { events, Job } = require("brigadier");
+events.on("exec", (e, p) => {
+    console.log("Received push for commit " + e.revision.commit)
+    var commit = e.revision.commit.substr(e.revision.commit.length - 7);
+    commit = e.revision.commit.substring(0, 7);
+    var greeting = new Job("job1", "alpine:latest");
+    greeting.storage.enabled = true;
+    greeting.tasks = [
+        "echo Hello Pipeline",
+        `echo commit id is ${commit}`
+
+    ]
+    var docker = new Job("job2" , "docker:dind");
+    docker.privileged = true;
+    docker.env = {
+    DOCKER_DRIVER: "overlay"
+    };
+docker.tasks = [
+    "dockerd-entrypoint.sh &",
+    "sleep 10",
+    "pwd",
+    "ls",
+    "ls -lart",
+    "cd /src/",
+    "pwd",
+    "ls -lart",
+    "pwd",
+    "docker build -t rahuldhus766/brigadeapp .",
+    "docker images",
+]
+
+   greeting.run();
+   docker.run();
+
+});
