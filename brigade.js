b89ef40807dd77309c0e039ed1b3eb49ccf1b48b
vivek-dev-26 <k.vivek@successive.tech>
2020-08-25 16:08:15 +0530

    remove docker push command

--- brigade.js
+++ brigade.js
@@ -27,19 +27,7 @@ docker.tasks = [
     "pwd",
     "docker build -t rahuldhus766/brigade:v1 .",
     "docker images",
-];
-
-// If a Docker user is specified, we push.
-if (project.secrets.DOCKER_USER) {
-  docker.env.DOCKER_USER = project.secrets.DOCKER_USER
-  docker.env.DOCKER_PASS = project.secrets.DOCKER_PASS
-  docker.env.DOCKER_REGISTRY = project.secrets.DOCKER_REGISTRY
-  docker.tasks.push("docker login -u $DOCKER_USER -p $DOCKER_PASS $DOCKER_REGISTRY")
-  docker.tasks.push("docker push rahuldhus766/brigade:v1")
-} else {
-  console.log("skipping push. DOCKER_USER is not set.");
-}
-
+]
    greeting.run();
    docker.run();
 
