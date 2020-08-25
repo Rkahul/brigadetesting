05979fd303a99d0f1462f036190dd641335830e0
vivek-dev-26 <k.vivek@successive.tech>
2020-08-26 00:04:37 +0530

    commit for job 3

--- brigade.js
+++ brigade.js
@@ -30,6 +30,10 @@ docker.tasks = [
     node.storage.enabled = true;
     node.tasks = [
         "pwd",
+	"apk --no-cache add curl",
+	"curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash",
+	"nvm install node",
+	"nvm use node",
         "cd /src/",
         "pwd",
         "ls -lart",
