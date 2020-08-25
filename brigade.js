33e56643a218da6ed78468c86e476bea4880d2cd
vivek-dev-26 <k.vivek@successive.tech>
2020-08-25 17:04:22 +0530

    change event

--- brigade.js
+++ brigade.js
@@ -1,5 +1,5 @@
 const { events, Job } = require("brigadier");
-events.on("exec", (e, p) => {
+events.on("push", (e, p) => {
     console.log("Received push for commit " + e.revision.commit)
     var commit = e.revision.commit.substr(e.revision.commit.length - 7);
     commit = e.revision.commit.substring(0, 7);
@@ -25,7 +25,7 @@ docker.tasks = [
     "pwd",
     "ls -lart",
     "pwd",
-    "docker build -t rahuldhus766/brigade:v2 .",
+    "docker build -t rahuldhus766/brigade:${commit} .",
     "docker images",
 ]
    greeting.run();
